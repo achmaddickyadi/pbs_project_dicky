8b9f82fdadcf47db26756b0e4d06e819b3d045b8
achmaddickyadi <129281281+achmaddickyadi@users.noreply.github.com>
2024-04-29 15:29:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'db_pakaian'
-})
+    host: 'sql6.freesqldatabase.com',
+    user: 'sql6702756',
+    password: 'gdwGEaas4K',
+    database: 'sql6702756'
+});
 
-module.exports = db
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to database');
+});
+
+db.on('error', (err) => {
+    console.error('Database error:', err);
+});
+
+module.exports = db;
